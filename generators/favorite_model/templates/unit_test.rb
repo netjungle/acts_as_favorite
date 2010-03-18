e14c522a29ca64dd9ef251221f125bf42b01dc47
Hummel <hummel@netjungle.de>
2010-03-18 21:05:14 +0100

    Removed "fixtures" from (empty) unit test template (doesn't work with rail 2.3.+)

--- generators/favorite_model/templates/unit_test.rb
+++ generators/favorite_model/templates/unit_test.rb
@@ -1,7 +1,6 @@
 require File.dirname(__FILE__) + '<%= '/..' * class_nesting_depth %>/../test_helper'
 
-class <%= class_name %>Test < Test::Unit::TestCase
-  fixtures :<%= table_name %>
+class <%= class_name %>Test < Test::Unit::TestCase  
 
   # Replace this with your real tests.
   def test_truth
